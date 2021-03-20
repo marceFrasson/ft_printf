574a4889a750aa41c78972f7a2721932e0286964
Marce <lightsxworld@gmail.com>
2021-03-20 19:46:45 -0300

    adjustments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 10:03:20 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/03/20 19:40:16 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/03/20 19:46:32 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,22 @@ int main(void)
 	
 	ft_printf("\n   pntr |%-20.25p|   string |%5s|   char |%-20.4c|   prcnt |%-20.5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
 	
-	ch = printf("   pntr |%-20.25p|   string |%s|   char |%-20.4c|   prcnt |%-20.5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
-	printf("\n\t %d\n\n", ch);
+	ch = printf("   pntr |%-20.25p|   string |%5s|   char |%-20.4c|   prcnt |%-20.5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
+
+	ft_printf("\n   pntr |%020.25p|   string |%-5s|   char |%20.4c|   prcnt |%20.5%|   digit |%15.10d|\n\n", &*str, str, 'w', 465782);
+	
+	ch = printf("   pntr |%020.25p|   string |%-5s|   char |%20.4c|   prcnt |%20.5%|   digit |%15.10d|\n\n", &*str, str, 'w', 465782);
+
+	ft_printf("\n   pntr |%20.5p|   string |%025s|   char |%-4.10c|   prcnt |%020.5%|   digit |%-15.10d|\n\n", &*str, str, 'w', 465782);
+	
+	ch = printf("   pntr |%20.5p|   string |%025s|   char |%-4.10c|   prcnt |%20.5%|   digit |%-15.10d|\n\n", &*str, str, 'w', 465782);
+
+	ft_printf("\n   pntr |%-5.20p|   string |%-5.15s|   char |%4.10c|   prcnt |%-5.25%|   digit |%015d|\n\n", &*str, str, 'w', 465782);
+	
+	ch = printf("   pntr |%-5.20p|   string |%-5.15s|   char |%4.10c|   prcnt |%-5.25%|   digit |%015d|\n\n", &*str, str, 'w', 465782);
+
+	ft_printf("\n   pntr |%05.25p|   string |%20.5s|   char |%020.4c|   prcnt |%05.25%|   digit |%010.15d|\n\n", &*str, str, 'w', 465782);
+	
+	ch = printf("   pntr |%05.25p|   string |%20.5s|   char |%020.4c|   prcnt |%05.25%|   digit |%010.15d|\n\n", &*str, str, 'w', 465782);
 	return (0);
 }
