36245ce38d2dec340ac218106e48f2bc32b28881
Marce <lightsxworld@gmail.com>
2021-03-21 13:41:49 -0300

    test

--- which_flag.c
+++ which_flag.c
@@ -14,10 +14,10 @@
 
 int			read_number(t_flags *flag, t_counter *count, const char *input)
 {
-	if (input[count->i] == 0 && flag->read_number == 0)
-	{
-		return (0);
-	}
+	// if (input[count->i] == 0 && flag->read_number == 0)
+	// {
+	// 	return (0);
+	// }
 	if (ft_isdigit((int)input[count->i]))
 	{
 		while (ft_isdigit((int)input[count->i]))
@@ -109,14 +109,23 @@ void		is_it_dot(t_flags *flag, t_counter *count, const char *input)
 	{
 		count->i++;
 		flag->dot = 1;
+		ft_putchar(count, '=');
+		ft_putnbr(count, flag->read_number);
+		ft_putchar(count, '=');
 		if (read_number(flag, count, input) == 1)
 		{
+			ft_putchar(count, '_');
+			ft_putnbr(count, flag->read_number);
+			ft_putchar(count, '_');
 			flag->precision = flag->read_number + 2;
 			flag->read_number = 0;
 			flag->read = 1;
 		}
 		if (input[count->i] == '%')
 		{
+			ft_putchar(count, '-');
+			ft_putnbr(count, flag->read_number);
+			ft_putchar(count, '-');
 			if (flag->read == 0)
 				ft_putchar(count, '%');
 			else
