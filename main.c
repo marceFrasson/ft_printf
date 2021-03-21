43df5c32851b5e52e094401f1905ae6ea03740e2
Marce <lightsxworld@gmail.com>
2021-03-21 08:08:16 -0300

    tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 10:03:20 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/03/21 08:07:01 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/03/21 08:08:05 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int main(void)
 	char *str = "ADFJK";
 	int ch;
 	
-	ft_printf("\n   pntr |%-20.25p|   string |%15s|   char |%-20.4c|   prcnt |%5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
+	ft_printf("\n   pntr |%20.25p|   string |%15s|   char |%-20.4c|   prcnt |%5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
 	
-	ch = printf("   pntr |%-20.25p|   string |%15s|   char |%-20.4c|   prcnt |%5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
+	ch = printf("   pntr |%20.25p|   string |%15s|   char |%-20.4c|   prcnt |%5%|   digit |%15d|\n\n", &*str, str, 'w', 465782);
 
 
 
@@ -29,9 +29,9 @@ int main(void)
 	ch = printf("   pntr |%020.25p|   string |%-15s|   char |%20.4c|   prcnt |%-5%|   digit |%015.15d|\n\n", &*str, str, 'w', 465782);
 
 
-	ft_printf("\n   pntr |%20.5p|   string |%025s|   char |%-4.10c|   prcnt |%5.2%|   digit |%-15.10d|\n\n", &*str, str, 'w', 465782);
+	ft_printf("\n   pntr |%20.5p|   string |%025s|   char |%-4.10c|   prcnt |%-5.2%|   digit |%-15.10d|\n\n", &*str, str, 'w', 465782);
 	
-	ch = printf("   pntr |%20.5p|   string |%025s|   char |%-4.10c|   prcnt |%5.2%|   digit |%-15.10d|\n\n", &*str, str, 'w', 465782);
+	ch = printf("   pntr |%20.5p|   string |%025s|   char |%-4.10c|   prcnt |%-5.2%|   digit |%-15.10d|\n\n", &*str, str, 'w', 465782);
 
 
 
