f72798b27f7bc92128e878545ea12e8a450c4fe8
Marce <lightsxworld@gmail.com>
2021-03-21 11:28:55 -0300

    negative number

--- which_flag.c
+++ which_flag.c
@@ -79,9 +79,15 @@ void		is_it_star(va_list args, t_flags *flag, t_counter *count, const char *inpu
 	{
 		flag->read_star = va_arg(args, int);
 		if (input[count->i - 1] == '.')
-			flag->precision = flag->read_star + 2;
+			if (flag->read_star >= 0)
+				flag->precision = flag->read_star + 2;
 		else
 			flag->width = flag->read_star;
+		if (flag->read_star < 0 && input[count->i - 1] != '.')
+		{
+			flag->dash = 1;
+			flag->width = flag->read_star * -1;
+		}
 		flag->read_star = 0;
 		count->i++;
 		if (input[count->i] == '%')
